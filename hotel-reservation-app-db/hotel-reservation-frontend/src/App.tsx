eabf905c97acbd6db2f33ec016d402864cae3454
Rakesh60 <rakeshgupta5353@gmail.com>
2024-07-12 17:38:56 +0530

    Implement authentication functionality for the web

--- hotel-reservation-app-db/hotel-reservation-frontend/src/App.tsx
+++ hotel-reservation-app-db/hotel-reservation-frontend/src/App.tsx
@@ -16,62 +16,6 @@ import LandingPage from "./pages/landing_page";
 import theme from "./theme";
 import ErrorPage from "./pages/error";
 
-export default function App() {
-  const [signedIn, setSignedIn] = useState(false);
-  const [user, setUser] = useState<User>({
-    email: "",
-    id: "",
-    name: "",
-    mobileNumber: "",
-  });
-  const [isAuthLoading, setIsAuthLoading] = useState(false);
-
-  if (isAuthLoading) {
-    return <div>User authenticating...</div>;
-  }
-
-  return (
-    <>
-      <ThemeProvider theme={theme}>
-        <UserContext.Provider value={user}>
-          <Header />
-          <ToastContainer />
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              flexGrow: 1,
-            }}
-          >
-            <BrowserRouter>
-              <Routes>
-                {/* rooms */}
-                <Route path="/" Component={LandingPage} />
-                <Route path="/rooms" Component={RoomListing} />
-                {/* reservations */}
-                <Route path="/reservations" Component={ReservationListing} />
-                {/* new reservation */}
-                <Route
-                  path="/reservations/new"
-                  Component={ReservationAddingPage}
-                />
-                {/* update reservation */}
-                <Route
-                  path="/reservations/change"
-                  Component={ReservationUpdatingPage}
-                />
-                <Route path="/error" Component={ErrorPage} />
-                {/* Otherwise, show not found page */}
-                <Route path="*" Component={NotFound} />
-              </Routes>
-            </BrowserRouter>
-          </div>
-        </UserContext.Provider>
-      </ThemeProvider>
-    </>
-  );
-}
 
 export default function App() {
   const [signedIn, setSignedIn] = useState(false);
@@ -116,3 +60,4 @@ export default function App() {
   }
   setIsAuthLoading(false);
   }, []);
+}
